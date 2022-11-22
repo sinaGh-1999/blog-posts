5e5716a65e72f03cb36f6c9836419a152983de6e
sina <sinaghorbanpour1378@gmail.com>
2022-11-22 18:45:12 +0330

    saving arow functions (reducers) in variables

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,4 +1,4 @@
-export default (state = [], action) => {
+const postReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH-POSTS":
       return action.payload;
@@ -6,3 +6,5 @@ export default (state = [], action) => {
       return state;
   }
 };
+
+export default postReducer;
